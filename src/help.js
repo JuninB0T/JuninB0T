246c7ac025d27e95df4fe9629200c9c0edb10146
JuninB0T <juninreportbot@gmail.com>
2021-02-10 18:55:32 -0300

    First commit

---
+++ src/help.js
@@ -0,0 +1,149 @@
+const help = (prefix) => {
+	return `「 *Junin BOT* 」
+
+◪ *INFO*
+  ❏ Prefix: 「  ${prefix}  」
+  ❏ Criador : Junin
+  ❏ Dúvidas chame o criador:
+https://instagram.com/_juninbot?utm_source=qr
+
+◪ *SOBRE*
+  │
+  ├─ ❏ ${prefix}blocklist
+  ├─ ❏ ${prefix}chatlist
+  ├─ ❏ ${prefix}ping
+  └─ ❏ ${prefix}bugreport
+
+◪ *FAZER*
+  │
+  ├─ ❏ ${prefix}sticker
+  ├─ ❏ ${prefix}fig
+  ├─ ❏ ${prefix}figgif
+  ├─ ❏ ${prefix}stickergif
+  ├─ ❏ ${prefix}toimg
+  ├─ ❏ ${prefix}tomp3
+  ├─ ❏ ${prefix}bpink
+  ├─ ❏ ${prefix}marvellogo
+  ├─ ❏ ${prefix}snowwrite
+  ├─ ❏ ${prefix}3dtext
+  ├─ ❏ ${prefix}ninjalogo
+  ├─ ❏ ${prefix}water
+  ├─ ❏ ${prefix}firetext
+  ├─ ❏ ${prefix}logowolf
+  ├─ ❏ ${prefix}logowolf2
+  ├─ ❏ ${prefix}phlogo
+  ├─ ❏ ${prefix}glitch
+  ├─ ❏ ${prefix}neonlogo
+  ├─ ❏ ${prefix}neonlogo2
+  ├─ ❏ ${prefix}lionlogo
+  ├─ ❏ ${prefix}shadow
+  ├─ ❏ ${prefix}burnpaper
+  ├─ ❏ ${prefix}lovepaper
+  ├─ ❏ ${prefix}woodblock
+  ├─ ❏ ${prefix}qowheart
+  ├─ ❏ ${prefix}mutgrass
+  ├─ ❏ ${prefix}undergocean
+  ├─ ❏ ${prefix}woodenboards
+  ├─ ❏ ${prefix}wolfmetal
+  ├─ ❏ ${prefix}metalictglow
+  ├─ ❏ ${prefix}8bit
+  ├─ ❏ ${prefix}herrypotter
+  ├─ ❏ ${prefix}pubglogo
+  └─ ❏ ${prefix}quotemaker
+
+◪ *MIDIA*
+  │
+  ├─ ❏ ${prefix}trendtwit
+  ├─ ❏ ${prefix}randomkpop
+  └─ ❏ ${prefix}ytsearch
+
+◪ *EDUCAÇÃO*
+  │
+  ├─ ❏ ${prefix}wiki
+  ├─ ❏ ${prefix}wikien
+  ├─ ❏ ${prefix}nulis
+  ├─ ❏ ${prefix}quotes
+  ├─ ❏ ${prefix}quotes2
+  └─ ❏ ${prefix}artinama
+
+◪ *PORCENTAGEMS*
+  │
+  ├─ ❏ ${prefix}apakah
+  ├─ ❏ ${prefix}kapankah
+  ├─ ❏ ${prefix}chance
+  └─ ❏ ${prefix}bisakah
+
+◪ *DOWNLOADER*
+  │
+  ├─ ❏ ${prefix}images
+  ├─ ❏ ${prefix}ytmp3
+  ├─ ❏ ${prefix}ytmp4
+  ├─ ❏ ${prefix}tiktok
+  └─ ❏ ${prefix}joox
+
+◪ *AUDIO*
+  │
+  ├─ ❏ ${prefix}play
+  └─ ❏ ${prefix}tts
+
+◪ *MÚSICA*
+  │
+  ├─ ❏ ${prefix}ytmp3
+  └─ ❏ ${prefix}play
+
+◪ *STALKER*
+  │
+  ├─ ❏ ${prefix}tiktokstalk
+  └─ ❏ ${prefix}igstalk
+
+◪ *ANIME*
+  │
+  ├─ ❏ ${prefix}neonime
+  ├─ ❏ ${prefix}pokemon
+  ├─ ❏ ${prefix}loli
+  ├─ ❏ ${prefix}waifu
+  ├─ ❏ ${prefix}randomanime
+  ├─ ❏ ${prefix}husbu
+  ├─ ❏ ${prefix}randomhentai
+  ├─ ❏ ${prefix}wait
+  └─ ❏ ${prefix}nekonime
+
+◪ *DIVERSÃO*
+  │
+  ├─ ❏ ${prefix}alay
+  ├─ ❏ ${prefix}gantengcek
+  ├─ ❏ ${prefix}watak
+  ├─ ❏ ${prefix}hobby
+  ├─ ❏ ${prefix}game
+  ├─ ❏ ${prefix}bucin
+  ├─ ❏ ${prefix}trust
+  ├─ ❏ ${prefix}dare
+  └─ ❏ ${prefix}simi
+
+◪ *INFORMAÇÃOS*
+  │
+  ├─ ❏ ${prefix}idioma
+  ├─ ❏ ${prefix}kodenegara
+  ├─ ❏ ${prefix}kbbi
+  └─ ❏ ${prefix}fakta
+
+◪ *DONO JUNIN*
+  │
+  ├─ ❏ ${prefix}setprefix
+  ├─ ❏ ${prefix}block
+  ├─ ❏ ${prefix}bc
+  ├─ ❏ ${prefix}bcgc
+  ├─ ❏ ${prefix}clone
+  └─ ❏ ${prefix}clearall
+
+◪ *OUTROS*
+  │
+  ├─ ❏ ${prefix}send
+  ├─ ❏ ${prefix}wame
+  ├─ ❏ ${prefix}virtex
+  ├─ ❏ ${prefix}afk
+  └─ ❏ ${prefix}timer
+`
+}
+
+exports.help = help
